3463bc046c8dd2d5d9b7cb153d8f2273c24a1b7f
Dhana Shunmugasundram <sldhana@gmail.com>
2012-10-22 22:37:20 -0700

    new additions to BST.java

--- BST.cs
+++ BST.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Algorithms
+namespace Algorithms2
 {
     /*--http://www.algolist.net/Data_structures/Binary_search_tree/Removal */
 
@@ -12,6 +12,7 @@ namespace Algorithms
         public Node<T> Left { get; set; }
         public Node<T> Right { get; set; }
         public T Element { get; set; }
+        public int Count { get; set; }
 
         public Node(T item)
         {
@@ -27,6 +28,68 @@ namespace Algorithms
         {
             root = null;
         }
+
+        //how many items in the tree
+        public int size()
+        {
+            return size(root);
+        }
+
+        //count all nodes in left subtree + nodes in right subtree + root
+        public int size(Node<T> node)
+        {
+            if (node == null)
+                return 0;
+            else
+                return node.Count;
+        }
+
+        //
+        public int height()
+        {
+            return height(root);
+        }
+
+        public int height(Node<T> node)
+        {
+            //empty tree has height of -1
+            if (node == null)
+                return -1;            
+           /*
+            * non leaf node has the height one more than the larger of its two children
+            * leaf nodes have height of 0
+            */
+            else
+                return Math.Max(height(node.Left), height(node.Right)) + 1;
+        }
+
+        public Node<T> add(T element)
+        {
+            root = add(element, root);
+            return root;
+        }
+
+        public Node<T> add(T element, Node<T> node)
+        {
+            if (node == null)
+            {
+                node = new Node<T>(element);
+            }
+            else if ((element as IComparable).CompareTo(node.Element) < 0)
+            {
+                node.Left = add(element, node.Left);
+            }
+            else if ((element as IComparable).CompareTo(node.Element) > 0)
+            {
+                node.Right = add(element, node.Right);
+            }
+
+            node.Count = 1 + size(node.Left) + size(node.Right);
+
+            return node;
+        }
+
+       
 
         public Node<T> find(T element)
         {
@@ -51,7 +114,7 @@ namespace Algorithms
 
         public Node<T> remove(T element)
         {
-            root = remove(element, root);
+            root = remove(element, root); 
             return root;
         }
 
@@ -62,7 +125,7 @@ namespace Algorithms
                 throw new Exception("Item wasn't found");
             }
             else if ((element as IComparable).CompareTo(node.Element) < 0)
-            {
+            {  
                 node.Left = remove(element, node.Left);
             }
             else if ((element as IComparable).CompareTo(node.Element) > 0)
@@ -88,6 +151,10 @@ namespace Algorithms
             return node;
         }
 
+        //this only does one node level
+        /* first check if left node is not null, if not null, removeMin of left node
+         * if node.left == null, then return right node
+         */
         public Node<T> removeMin(Node<T> node)
         {
             if (node == null)
@@ -123,17 +190,13 @@ namespace Algorithms
             }
         }
 
-        public Node<T> add(T element)
-        {
-            root = add(element, root);
-            return root;
-        }
-
+       
         public Node<T> findMin()
         {
             return findMin(root);
         }
 
+        //since it's a BST, you know left nodes will contain the smallest values.
         public Node<T> findMin(Node<T> node)
         {
             if (node != null)
@@ -167,24 +230,6 @@ namespace Algorithms
             2. If less: if current node has no left child, add there. Else, handle left child with same algorithm.
          *  3. If more: if current node has no right child, add there. Else repeat.
          */
-
-        public Node<T> add(T element, Node<T> node)
-        {
-            if (node == null)
-            {
-                node = new Node<T>(element);
-            }
-            else if ((element as IComparable).CompareTo(node.Element) < 0)
-            {
-                node.Left = add(element, node.Left);
-            }
-            else if ((element as IComparable).CompareTo(node.Element) > 0)
-            {
-                node.Right = add(element, node.Right);
-            }
-
-            return node;
-        }
 
        
         /*
@@ -232,7 +277,11 @@ namespace Algorithms
             Console.WriteLine(node.Element);
         }
 
-        //VLR (value, left, right, check the value, go recursively down the left node, get all the lefts, then the rights
+        /*VLR (value, left, right
+         * Start at root, get value, go left, get value, go down recursively. 
+         * When no more lefts, go up, go right, print value, repeat above.
+        */
+ 
         public void preorderTraversal()
         {
             preorderTraversal(root);
@@ -241,7 +290,7 @@ namespace Algorithms
 
         public void preorderTraversal(Node<T> node)
         {
-            Console.WriteLine(node.Element);
+            Console.WriteLine(node.Element); 
             if (node.Left != null)
             {
                 preorderTraversal(node.Left);
@@ -279,6 +328,40 @@ namespace Algorithms
         }
 
 
+        /*
+         * An empty tree is height balanced.
+         * A non empty tree is balanced if:
+         *  a) Left subtree is balanced
+         *  b) Right subtree is balanced
+         *  c) The difference between height of left subtree and right subtree is no more than 1.
+         */
+        public bool isBalancedTree()
+        {
+            return isBalancedTree(root);
+        }
+
+        /* To check if a tree is balanced, get the height of left and right subtree.  Return true if difference between
+         * heights is no more than 1 and left and right subtrees are balanced.
+         */
+        public bool isBalancedTree(Node<T> node)
+        {
+            if (node == null)            
+                return true;
+
+
+            int leftTreeHeight = height(node.Left);
+            int rightTreeHeight = height(node.Right);
+
+            if (Math.Abs(leftTreeHeight - rightTreeHeight) <= 1
+                && isBalancedTree(node.Left)
+                && isBalancedTree(node.Right))
+                return true;
+
+            //tree not balanced
+            return false;
+        }
+
+
         public static void main()
         {
             BST<int> bst = new BST<int>();
@@ -293,7 +376,9 @@ namespace Algorithms
             //bst.inorderTraversal();  /* 1 4 31 200 500 */
             //bst.postorderTraversal();  /* 4 31 1 500 200 */
             //bst.preorderTraversal();  /* 200 1 31 4 500 */
-            bst.breadthFirstTraversal(); /* 200 1 500 31 4 */
+            //bst.breadthFirstTraversal(); /* 200 1 500 31 4 */
+            bst.isBalancedTree();
+
 
 
         }
