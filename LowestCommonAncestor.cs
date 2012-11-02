f10c6090b796f4655dfa928fcf2b3b4b825794b5
Dhana Shunmugasundram <sldhana@gmail.com>
2012-11-01 19:46:25 -0700

    lowest common ancestor code

--- LowestCommonAncestor.cs
+++ LowestCommonAncestor.cs
@@ -2,41 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Algorithms
 {
     /*
-     * 1.  If current node is null or one of the nodes we need to find for LCA is null, then return null.
-     * 2.  If both nodes are present in left subtree, call left subtree.  Repeat for right subtree.
-     */
+      * 1.  If current node is null or one of the nodes we need to find for LCA is null, then return null.
+      * 2.  If both nodes are present in left subtree, call left subtree.  Repeat for right subtree.
+      */
 
     public static class LowestCommonAncestor
     {
         public static Node<int> FindCommonAncestor(BST<int> tree, int i, int j)
         {
-            //if either node is root, there cannot be a common parent
-            if (tree.getRoot().Element == i || tree.getRoot().Element == j)
-            {
-                return null;
-            }
             return FindCommonAncestor(tree.getRoot(), i, j);
         }
 
         public static Node<int> FindCommonAncestor(Node<int> node, int i, int j)
         {
-            
-            if (Contains(node.Left, i) && Contains(node.Left, j))
+
+            if (node == null)
             {
-                return FindCommonAncestor(node.Left, i, j);
+                return null;
+            }
+            else if (node.Left != null && (node.Left.Element == i || node.Left.Element == j))
+            {
+                return node;
+            }
+            else if (node.Right != null && (node.Right.Element == i || node.Right.Element == j))
+            {
+                return node;
             }
 
-            if (Contains(node.Right, i) && Contains(node.Right, j))
+            else
             {
-                return FindCommonAncestor(node.Right, i, j);
-            }
+                Node<int> left = FindCommonAncestor(node.Left, i, j);
+                Node<int> right = FindCommonAncestor(node.Right, i, j);
 
-            return node;
+                if (left != null && right != null)
+                {
+                    return node;
+                }
+                else
+                {
+
+                    Node<int> elem = left == null ? right : left;
+                    return elem;
+                }
+            }
         }
 
         private static Boolean Contains(Node<int> node, int elem)
@@ -71,13 +83,19 @@ namespace Algorithms
             //bst.breadthFirstTraversal();
 
             Node<int> node = LowestCommonAncestor.FindCommonAncestor(bst, 4, 14);
-            Console.WriteLine(node.Element);
+            Console.WriteLine(node.Element);//8
 
-            Node<int> node2 = LowestCommonAncestor.FindCommonAncestor(bst, 12, 300);
-            Console.WriteLine(node2.Element);
+            Node<int> node2 = LowestCommonAncestor.FindCommonAncestor(bst, 10, 14);
+            Console.WriteLine(node2.Element); //12
 
-            //Node<int> node3 = LeastCommonAncestor.FindCommonAncestor(bst, 30, 31);
-            // Console.WriteLine(node3.Element);
+            Node<int> node3 = LowestCommonAncestor.FindCommonAncestor(bst, 8, 22);
+            Console.WriteLine(node3.Element); //20
+
+            Node<int> node4 = LowestCommonAncestor.FindCommonAncestor(bst, 4, 22);
+            Console.WriteLine(node4.Element);//20
+
+            Node<int> node5 = LowestCommonAncestor.FindCommonAncestor(bst, 4, 8);
+            Console.WriteLine(node5.Element); //20
         }
     }
 }
