960fc09039cc651f4b9b5684aded9d39bc257200
Dhana Shunmugasundram <sldhana@gmail.com>
2012-11-06 08:03:43 -0800

    adding partial code for graph.cs

---
+++ Graph.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Vertex
+    {
+        public bool wasVisited;
+        public string label;
+
+        public Vertex(string l)
+        {
+            label = l;
+            wasVisited = false;
+        }
+    }
+
+    public class Graph
+    {
+        private int totalVertices = 0;
+        private int totalEdges;
+        private ArrayList[] adjacentMatrix;
+        
+
+        public Graph(int v)
+        {
+            if (v < 0)
+            {
+                throw new ArgumentException("Number of vertices has to be greater than 0");
+            }
+
+            totalVertices = v;
+            totalEdges = 0;
+            adjacentMatrix = new ArrayList[totalVertices];
+            for (int i = 0; i < totalVertices; i++)
+            {
+                adjacentMatrix[i] = new ArrayList();
+            }
+        }
+
+        public void AddEdge(int start, int end)
+        {
+            if ((start < 0 || start >= totalVertices) || (end < 0 || end >= totalVertices) { 
+                throw new IndexOutOfRangeException;
+            }
+            totalEdges++;
+            adjacentMatrix[start].Add(end);
+            adjacentMatrix[end].Add(start);
+
+        }
+    }
+
+    
+}
