62779996887aec783d8832f9cba2454fe8b6c632
Dhana Shunmugasundram <sldhana@gmail.com>
2012-11-12 22:35:23 -0800

    more graph questions

--- Program.cs
+++ Program.cs
@@ -10,54 +10,27 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            UndirectedGraph g = new UndirectedGraph(13);
-            g.AddEdge(0, 6);
+            UndirectedGraph g = new UndirectedGraph(6);
             g.AddEdge(0, 2);
             g.AddEdge(0, 1);
             g.AddEdge(0, 5);
             g.AddEdge(1, 0);
+            g.AddEdge(1, 2);
             g.AddEdge(2, 0);
+            g.AddEdge(2, 1);
+            g.AddEdge(2, 3);
+            g.AddEdge(2, 4);
             g.AddEdge(3, 5);
             g.AddEdge(3, 4);
-            g.AddEdge(4, 5);
-            g.AddEdge(4, 6);
+            g.AddEdge(3, 2);
             g.AddEdge(4, 3);
+            g.AddEdge(4, 2);
             g.AddEdge(5, 3);
-            g.AddEdge(5, 4);
             g.AddEdge(5, 0);
-            g.AddEdge(6, 0);
-            g.AddEdge(6, 4);
-            g.AddEdge(7, 8);
-            g.AddEdge(8, 7);
-            g.AddEdge(9, 11);
-            g.AddEdge(9, 10);
-            g.AddEdge(9, 12);
-            g.AddEdge(10, 9);
-            g.AddEdge(11, 9);
-            g.AddEdge(11, 12);
-            g.AddEdge(12, 11);
-            g.AddEdge(12, 9);
 
-            UndirectedGraphDepthFirst depthFirst = new UndirectedGraphDepthFirst(g);
-            depthFirst.search(g, 8);
-
-            Queue<int> q = depthFirst.getPath();
-            Console.WriteLine("Path is: ");
-            foreach (int value in q)
-            {
-                Console.Write(value + " ");
-            }
-
-
-            for (int i = 0; i < g.getTotalEdges(); i++)
-            {
-                if (depthFirst.pathExists(i) != false)
-                {
-                    Console.Write(i);
-                }
-            }
-
-            Console.WriteLine("Connected: " + depthFirst.isConnected(g));
+            UndirectedGraphBreadthFirst br = new UndirectedGraphBreadthFirst(g);
+            //int[] shortestPath = br.getShortestPath(g, 0);
+            br.bfs(g, 5);
         }
     }
 }
--- UndirectedGraphBreadthFirst.cs
+++ UndirectedGraphBreadthFirst.cs
@@ -9,16 +9,69 @@ namespace Algorithms
 {
     public class UndirectedGraphBreadthFirst
     {
+        /*
+         * 1)all vertices of G are first painted white
+	       2)the graph root is painted gray and put in a queue
+           3)while the queue is not empty{
+	        a) a vertex u is removed from the queue
+            b) for all white successors v of u{
+	            i. v is painted gray
+		        ii. v is added to the queue
+	        }
+	        c) u is painted black
+	       }
+         */
+
         private bool[] marked;
 
         private int[] distanceMatrix; //number of edges in shortest path
         private int[] pathMatrix; //previous edge in shortest path
+
+        private int[] visitedMatrix;
 
         public UndirectedGraphBreadthFirst(UndirectedGraph g)
         {
             marked = new bool[g.getTotalVertices()];
             distanceMatrix = new int[g.getTotalVertices()];
             pathMatrix = new int[g.getTotalVertices()];
+            visitedMatrix = new int[g.getTotalVertices()];
+        }
+
+        public void bfs(UndirectedGraph g, int vertex)
+        {
+            Queue<int> q = new Queue<int>();
+
+            //set up the entire visitedMatrix with -1 except for the vertex node
+            for (int i = 0; i < g.getTotalVertices(); i++)
+            {
+                visitedMatrix[i] = -1;
+            }
+            
+            //mark root with 1 to sat it's visited
+            visitedMatrix[vertex] = 1;
+            q.Enqueue(vertex);
+
+            while (q.Count > 0)
+            {
+                int v = q.Dequeue();
+                Console.WriteLine(v);
+                marked[v] = true;
+
+                //get all adjacent
+                ArrayList adj = g.Adjacent(v);
+                
+                for (int i = 0; i < adj.Count; i++)
+                {
+                    int currentVertex = (int) adj[i];
+                    if (visitedMatrix[currentVertex] == -1)
+                    {
+                        q.Enqueue(currentVertex);
+                        visitedMatrix[currentVertex] = 1;
+                    }
+                }
+                visitedMatrix[v] = 0;
+                marked[v] = false;
+            }
         }
 
         public int[] getShortestPath(UndirectedGraph g, int vertex)
@@ -77,8 +130,8 @@ namespace Algorithms
             g.AddEdge(5, 0);
 
             UndirectedGraphBreadthFirst br = new UndirectedGraphBreadthFirst(g);
-            int[] shortestPath = br.getShortestPath(g, 0);
-
+            //int[] shortestPath = br.getShortestPath(g, 0);
+            br.bfs(g, 5);
 
         }
     }
